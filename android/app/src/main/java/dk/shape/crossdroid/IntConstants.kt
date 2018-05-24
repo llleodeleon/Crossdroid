16621760c32ba5addc764572dc274204a6926449
kerembalcan <kerembalcan@hotmail.com>
2018-05-24 20:56:40 +0200

    Solved issues on react activity

---
+++ android/app/src/main/java/dk/shape/crossdroid/IntConstants.kt
@@ -0,0 +1,5 @@
+package dk.shape.crossdroid
+
+object IntConstants  {
+    const val OVERLAY_PERMISSION_REQ_CODE = 100
+}
--- android/app/src/main/java/dk/shape/crossdroid/MainReactActivity.kt
+++ android/app/src/main/java/dk/shape/crossdroid/MainReactActivity.kt
@@ -2,18 +2,24 @@ package dk.shape.crossdroid
 
 import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactRootView
-import com.facebook.react.LifecycleState
 import com.facebook.react.shell.MainReactPackage
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.facebook.react.ReactActivity
 import com.facebook.react.common.LifecycleState
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import android.view.KeyEvent
+import dk.shape.crossdroid.IntConstants.OVERLAY_PERMISSION_REQ_CODE
+
+
 
 
 class MainReactActivity : ReactActivity(), DefaultHardwareBackBtnHandler {
     private var mReactRootView: ReactRootView? = null
-    private var mReactInstanceManager: ReactInstanceManager? = null
+    private lateinit var mReactInstanceManager: ReactInstanceManager
 
     override fun onCreate(savedInstanceState: Bundle) {
         super.onCreate(savedInstanceState)
@@ -22,7 +28,7 @@ class MainReactActivity : ReactActivity(), DefaultHardwareBackBtnHandler {
         mReactInstanceManager = ReactInstanceManager.builder()
                 .setApplication(application)
                 .setBundleAssetName("index.android.bundle")
-                .setJSMainModulePath("index")
+                .setJSMainModuleName("index")
                 .addPackage(MainReactPackage())
                 .setUseDeveloperSupport(BuildConfig.DEBUG)
                 .setInitialLifecycleState(LifecycleState.RESUMED)
@@ -32,9 +38,55 @@ class MainReactActivity : ReactActivity(), DefaultHardwareBackBtnHandler {
         mReactRootView!!.startReactApplication(mReactInstanceManager, "RNCrossdroid", null)
 
         setContentView(mReactRootView)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:$packageName"))
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE)
+            }
+        }
     }
 
     override fun invokeDefaultOnBackPressed() {
         super.onBackPressed()
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    // SYSTEM_ALERT_WINDOW permission not granted...
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mReactInstanceManager.onHostPause(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mReactInstanceManager.onHostResume(this, this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mReactInstanceManager.onHostDestroy(this)
+    }
+
+    override fun onBackPressed() {
+            mReactInstanceManager.onBackPressed()
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_MENU) {
+            mReactInstanceManager.showDevOptionsDialog()
+            return true
+        }
+        return super.onKeyUp(keyCode, event)
+    }
+
 }
